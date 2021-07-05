7383660d60edaf8bd28ee8e80915096a4e782951
Ajith <58944893+ajith-m-doodlebug@users.noreply.github.com>
2021-07-05 19:49:22 +0530

    Add files via upload

---
+++ coolme.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api',methods=['GET'])
+def hello():
+    d={}
+    d['Query'] = str(request.args['Query'])
+    return jsonify(d)
+
+if __name__ == '__main__':
+    app.run()
